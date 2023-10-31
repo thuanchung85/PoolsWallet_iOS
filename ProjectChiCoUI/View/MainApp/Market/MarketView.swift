190f9a104d2dd1ee3b0c0dc8669962c147383b2a
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-31 13:15:41 +0700

    save

---
+++ ProjectChiCoUI/View/MainApp/Market/MarketView.swift
@@ -0,0 +1,20 @@
+//
+//  WalletView.swift
+//  PoolsWallet_iOS
+//
+//  Created by SWEET HOME (^0^)!!! on 31/10/2023.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+public struct MarketView: View {
+    
+    //==BODY==//
+    public var body: some View{
+        Spacer()
+        Text("MarketView")
+        Spacer()
+    }//end body
+    
+}//end struct
