c13ac3e16e4b7355fdd897b7262b156d72cb7ede
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-31 11:16:52 +0700

    save

--- ProjectChiCoUI/View/MainApp/MainApp.swift
+++ ProjectChiCoUI/View/MainApp/MainApp.swift
@@ -10,6 +10,8 @@ public struct MainApp: View {
     @Binding var walletAddress:String
     @Binding var walletName:String
     
+    @State var currentChoose = 1
+    
     //==INIT==//
     public init(walletName:Binding<String>, walletAddress:Binding<String>) {
         self._walletAddress = walletAddress
@@ -29,81 +31,152 @@ public struct MainApp: View {
                 }
                 
                 //khu menu 4 nut bên dưới
-                HStack(alignment: .bottom, spacing: 50){
+                HStack(){
                     ZStack{
                         Rectangle()
                             .fill(.white)
-                            .frame(height: 100)
+                            .frame(height: 70)
                             .background(
                                 Color.white // any non-transparent background
-                                    .shadow(color: Color.gray, radius: 10, x: 0, y: 0)
+                                    .shadow(color: Color.gray, radius: 5, x: 0, y: 0)
                                     .mask(Rectangle().padding(.top, -20)) /// here!
                             )
                         
-                        HStack(alignment: .bottom, spacing: 50){
+                        HStack(alignment: .bottom, spacing: 65){
                             Button(action: {
-                                
+                                currentChoose = 1
                             }) {
-                                VStack{
-                                    Image("wallet")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 30)
-                                    Text("Wallet")
-                                        .font(.custom("Arial", size: 15))
-                                        .foregroundColor(.gray)
+                                if(self.currentChoose == 1){
+                                    VStack{
+                                        Image("wallet")
+                                            .renderingMode(.template)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .foregroundColor(.green)
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 25)
+                                            
+                                        Text("Wallet")
+                                            .font(.custom("Arial", size: 10))
+                                            .foregroundColor(.green)
+                                    }
+                                }
+                                else{
+                                    VStack{
+                                        Image("wallet")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 25)
+                                            .foregroundColor(.gray)
+                                        Text("Wallet")
+                                            .font(.custom("Arial", size: 10))
+                                            .foregroundColor(.gray)
+                                    }
                                 }
                             }
                             
                             Button(action: {
-                                
+                                currentChoose = 2
                             }) {
-                                VStack{
-                                    Image("market")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 30)
-                                    Text("Market")
-                                        .font(.custom("Arial", size: 15))
-                                        .foregroundColor(.gray)
+                                if(self.currentChoose == 2){
+                                    VStack{
+                                        Image("market")
+                                            .renderingMode(.template)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .foregroundColor(.green)
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 25)
+                                            
+                                        Text("Market")
+                                            .font(.custom("Arial", size: 10))
+                                            .foregroundColor(.green)
+                                    }
+                                }
+                                else{
+                                    VStack{
+                                        Image("market")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 25)
+                                            .foregroundColor(.gray)
+                                        Text("Market")
+                                            .font(.custom("Arial", size: 10))
+                                            .foregroundColor(.gray)
+                                    }
                                 }
                             }
                             
                             Button(action: {
-                                
+                                currentChoose = 3
                             }) {
-                                VStack{
-                                    Image("mining")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 30)
-                                    Text("Mining")
-                                        .font(.custom("Arial", size: 15))
-                                        .foregroundColor(.gray)
+                                if(self.currentChoose == 3){
+                                    VStack{
+                                        Image("mining")
+                                            .renderingMode(.template)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .foregroundColor(.green)
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 25)
+                                           
+                                        Text("Mining")
+                                            .font(.custom("Arial", size: 10))
+                                            .foregroundColor(.green)
+                                    }
+                                }else{
+                                    VStack{
+                                        Image("mining")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 25)
+                                            .foregroundColor(.gray)
+                                        Text("Mining")
+                                            .font(.custom("Arial", size: 10))
+                                            .foregroundColor(.gray)
+                                    }
                                 }
                             }
                             
                             Button(action: {
-                                
+                                currentChoose = 4
                             }) {
-                                VStack{
-                                    Image("setting")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 30)
-                                    Text("Setting")
-                                        .font(.custom("Arial", size: 15))
-                                        .foregroundColor(.gray)
+                                if(self.currentChoose == 4){
+                                    VStack{
+                                        Image("setting")
+                                            .renderingMode(.template)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .foregroundColor(.green)
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 25)
+                                           
+                                        Text("Setting")
+                                            .font(.custom("Arial", size: 10))
+                                            .foregroundColor(.green)
+                                    }
+                                }
+                                else{
+                                    VStack{
+                                        Image("setting")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 25)
+                                            .foregroundColor(.gray)
+                                        Text("Setting")
+                                            .font(.custom("Arial", size: 10))
+                                            .foregroundColor(.gray)
+                                    }
                                 }
                             }
                         }
                     }
                 }
-                .frame(maxWidth: .infinity, maxHeight: 100)
+               
                 
                 
             }
